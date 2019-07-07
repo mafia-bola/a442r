c1f8bfd056991a34e222b0c51a76ad1ddb137b7e
rizky_ananda_ds <rizky.ananda.ds@gmail.com>
2019-07-07 17:01:02 +0800

    Fixing View Login

--- app/src/main/java/com/android/kecak/HistoryActivity.java
+++ app/src/main/java/com/android/kecak/HistoryActivity.java
@@ -2,9 +2,17 @@ package com.android.kecak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        Intent back = new Intent(HistoryActivity.this, MenuActivity.class);
+        startActivity(back);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
