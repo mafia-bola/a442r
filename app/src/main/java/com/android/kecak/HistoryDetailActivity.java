c279962da708a606df16d6cb9d5548fb3897f220
rizky_ananda_ds <rizky.ananda.ds@gmail.com>
2019-07-07 20:19:06 +0800

    Fix Image

--- app/src/main/java/com/android/kecak/HistoryDetailActivity.java
+++ app/src/main/java/com/android/kecak/HistoryDetailActivity.java
@@ -1,10 +1,13 @@
 package com.android.kecak;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class HistoryDetailActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
