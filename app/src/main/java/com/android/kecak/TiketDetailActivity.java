0003e3874601e429eae5d436914a6b545fa8deec
rizky.ananda.ds <rizky.ananda.ds@gmail.com>
2019-08-28 13:44:50 +0800

    Hapus Pemesanan

--- app/src/main/java/com/android/kecak/TiketDetailActivity.java
+++ app/src/main/java/com/android/kecak/TiketDetailActivity.java
@@ -1,19 +1,36 @@
 package com.android.kecak;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.github.barteksc.pdfviewer.PDFView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TiketDetailActivity extends AppCompatActivity {
 
@@ -60,5 +77,83 @@ public class TiketDetailActivity extends AppCompatActivity {
         protected void onPostExecute(InputStream inputStream) {
             pdfView.fromStream(inputStream).load();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_hapus, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.btnDelete:
+                AlertDialog.Builder alert = new AlertDialog.Builder(TiketDetailActivity.this);
+                alert
+                        .setTitle("Batalkan Pemesanan")
+                        .setMessage("Apakan anda akan menghapus tiket ini ?")
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                batalPemesanan();
+                                Intent batal = new Intent(TiketDetailActivity.this, TiketActivity.class);
+                                startActivity(batal);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void batalPemesanan() {
+        Intent history = getIntent();
+        final long id_pemesanan = history.getLongExtra("id_pemesanan",0);
+
+        String urlAddress = getString(R.string.urlAddress);
+        final String hapusPemesanan = urlAddress+"api/batalpesan/"+id_pemesanan;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, hapusPemesanan,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String status = jsonObject.getString("status");
+
+                            if (status.equals("sukses")){
+                                Toast.makeText(TiketDetailActivity.this, "Tiket telah dihapus", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(TiketDetailActivity.this, "Gagal melakukan penghapusan tiket", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(TiketDetailActivity.this, "Gagal melakukan penghapusan tiket", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id", String.valueOf(id_pemesanan));
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
